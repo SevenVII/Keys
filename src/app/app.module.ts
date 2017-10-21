96d378a2d05b4e84b60e0095f632fe3b1e03d091
SevenVII <guillermo.jose.garcia.gonzalez@gmail.com>
2017-10-21 09:45:46 +0200

    Remodeling of app files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header.component';
+import { BoxComponent } from './box.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    BoxComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/box.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'box-keys',
+  templateUrl: './box.component.html',
+  styleUrls: ['./box.component.css']
+})
+
+export class BoxComponent implements OnInit {
+  ngOnInit(): void {
+  }
+}
