cb0c4a495a6c8434aec32da2e1f0de5a281966b7
Seven <guillermo.jose.garcia.gonzalez@gmail.com>
2018-03-18 06:56:13 +0100

    Changed static table to jquery generated table

--- src/app/box.component.ts
+++ src/app/box.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'box-keys',
@@ -7,6 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class BoxComponent implements OnInit {
-  ngOnInit(): void {
+  public ngOnInit(): void {
+
+    for (let i = 0; i < 11; i++) {
+      $('#tableCode').append('<tr></tr>');
+    }
+
+    for (let i = 0; i < 10; i++) {
+      $('#tableCode tr:nth-child(' + i + ')').each(function(){
+        for (let j = 0; j < 10; j++) {
+          $(this).append('<td></td>');
+        }
+      });
+    }
+
+    $('#tableCode').css('border', '1px solid black');
   }
+
 }
