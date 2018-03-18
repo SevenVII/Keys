e40e76dc470ce1ebf5083acebf30f422ac01855a
Seven <guillermo.jose.garcia.gonzalez@gmail.com>
2018-03-18 08:30:06 +0100

    Dynamic table created. UI near to be finished

--- src/app/box.component.ts
+++ src/app/box.component.ts
@@ -1,28 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import * as $ from 'jquery';
 
 @Component({
   selector: 'box-keys',
   templateUrl: './box.component.html',
-  styleUrls: ['./box.component.css']
+  styleUrls: ['./box.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 
 export class BoxComponent implements OnInit {
   public ngOnInit(): void {
 
-    for (let i = 0; i < 11; i++) {
+    for (let i = 0; i < 10; i++) {
       $('#tableCode').append('<tr></tr>');
     }
 
-    for (let i = 0; i < 10; i++) {
+    let cont = 0;
+
+    for (let i = 0; i < 11; i++) {
       $('#tableCode tr:nth-child(' + i + ')').each(function(){
         for (let j = 0; j < 10; j++) {
-          $(this).append('<td></td>');
+          $(this).append('<td id="cell' + cont + '"></td>');
+          cont++;
         }
       });
     }
-
-    $('#tableCode').css('border', '1px solid black');
   }
 
 }
